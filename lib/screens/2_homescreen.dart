6308206003aba847bc68ce8eb72778faef24e1a6
farida <98031733+IFarida@users.noreply.github.com>
2022-05-09 16:36:14 +0300

    tos_parkoviy_app

--- lib/screens/2_homescreen.dart
+++ lib/screens/2_homescreen.dart
@@ -1,193 +1,213 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
-import '../components/constants.dart';
+import '../../components/constants.dart';
 import 'package:tos_parkoviy_app/screens/3_catalog/5_section_map.dart';
 
-// void main() => runApp(const MyApp());
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-//
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'Список',
-//       theme: ThemeData(
-//         fontFamily: "Nunito",
-//         scaffoldBackgroundColor: kBackgroundColor,
-//         textTheme: Theme.of(context).textTheme.apply(displayColor: kTextColor),
-//       ),
-//       home: const HomeScreen(),
-//     );
-//   }
-// }
-
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen ({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    var size = MediaQuery.of(context).size;
-    //give us total height and with our phone
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          bottomSheetTheme: const BottomSheetThemeData(
+            backgroundColor: Colors.white,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.vertical(top: Radius.circular(20))),
+          ),
+          textTheme: const TextTheme(
+            caption: TextStyle(fontSize: 20, color: Colors.white),
+          ),
+          fontFamily: 'Lato-Black'),
+      home: const ProfileScreen(),
+    );
+  }
+}
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          backgroundColor: Color.fromARGB(255, 4, 144, 76),
-          elevation: 0,
-          leading: IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.menu),
-          ),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.person),
-            )
-          ]),
-      body: Stack(
-        children: <Widget>[
-          Container(
-            height: size.height * .45,
-            decoration: const BoxDecoration(
-              color: Color.fromARGB(255, 184, 245, 185),
-            ),
-          ),
-          SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                children: <Widget>[
-                  Text(
-                    "Tos Parkoviy",
-                    style: Theme.of(context)
-                        .textTheme
-                        .displaySmall
-                        ?.copyWith(fontWeight: FontWeight.w700),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.symmetric(vertical: 30),
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 30, vertical: 5),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(29.5),
-                    ),
-                    child: const TextField(
-                      decoration: InputDecoration(
-                        hintText: "Search",
-                        icon: Icon(
-                          Icons.search,
-                          size: 20,
-                          color: Colors.black12,
-                        ),
-                        border: InputBorder.none,
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: GridView.count(
-                      crossAxisCount: 2,
-                      childAspectRatio: .85,
-                      crossAxisSpacing: 20,
-                      mainAxisSpacing: 20,
-                      children: <Widget>[
-                        CategoryCard(
-                          title: "Дома",
-                          images: "assets/images/4.png",
-                          press: () {},
-                        ),
-                        CategoryCard(
-                          title: "Мероприятия",
-                          images: "assets/images/1.png",
-                          press: () {},
-                        ),
-                        CategoryCard(
-                          title: "Пространства",
-                          images: "assets/images/3.png",
-                          press: () {},
-                        ),
-                        CategoryCard(
-                          title: "Организации",
-                          images: "assets/images/2.png",
-                          press: () {},
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    child: TextButton(
-                      child: Text("Карта локаций"),
-                      onPressed: () {
-                        Route route =
-                            MaterialPageRoute(builder: ((context) => TOSMap()));
-                        Navigator.push(context, route);
-                      },
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
-        ],
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: const AssetImage('assets/images/backgroundimage.jpg'),
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(
+                Colors.white.withOpacity(0.5),
+                BlendMode.colorDodge,
+              )),
+        ),
+        child: const Body(),
       ),
     );
   }
 }
 
-class CategoryCard extends StatelessWidget {
-  final String title;
-  final String images;
-  final Function press;
-  const CategoryCard({
-    Key? key,
-    required this.title,
-    required this.images,
-    required this.press,
-  }) : super(key: key);
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
+
+  hexColor(String colorhexcode) {
+    String colornew = '0xff' + colorhexcode;
+    colornew = colornew.replaceAll('#', '');
+    int colorint = int.parse(colornew);
+    return colorint;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(13),
-      child: Container(
-        //padding: const EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(13),
-          boxShadow: const [
-            BoxShadow(
-              offset: Offset(0, 17),
-              blurRadius: 17,
-              spreadRadius: -23,
-              color: kShadowColor,
-            ),
-          ],
-        ),
-        child: Material(
-          color: Colors.transparent,
-          child: InkWell(
-            onTap: () =>
-                {Navigator.pushNamed(context, '/catalog', arguments: title)},
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                children: <Widget>[
-                  const Spacer(),
-                  const Spacer(),
-                  Text(
-                    title,
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context)
-                        .textTheme
-                        .titleSmall
-                        ?.copyWith(fontSize: 15),
-                  )
+    return Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            children: [
+              Text(
+                'Выберите раздел',
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Color(hexColor('#00008B')),
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: RaisedButton(
+              elevation: 20,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(150)),
+              color: bgColorHousesAppBar,
+              onPressed: () {},
+              child: Row(
+                children: [
+                  Image.asset(
+                    'assets/icons/houses.png',
+                    width: 60,
+                    height: 70,
+                  ),
+                  const SizedBox(width: 25),
+                  const Expanded(
+                    child: Text(
+                      'Дом',
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                  ),
                 ],
               ),
             ),
           ),
-        ),
-      ),
-    );
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: RaisedButton(
+              elevation: 20,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(150)),
+              color: bgColorEventsAppBar,
+              onPressed: () {},
+              child: Row(
+                children: [
+                  Image.asset(
+                    'assets/icons/events.png',
+                    width: 60,
+                    height: 70,
+                  ),
+                  const SizedBox(width: 25),
+                  const Expanded(
+                    child: Text(
+                      'Мероприятия',
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: RaisedButton(
+              elevation: 20,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(150)),
+              color: bgColorPlacesAppBar,
+              onPressed: () {},
+              child: Row(
+                children: [
+                  Image.asset(
+                    'assets/icons/locations.png',
+                    width: 60,
+                    height: 70,
+                  ),
+                  const SizedBox(width: 25),
+                  const Expanded(
+                    child: Text(
+                      'Пространства',
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: RaisedButton(
+              elevation: 20,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(150)),
+              color: bgColorOrganizationsAppBar,
+              onPressed: () {},
+              child: Row(
+                children: [
+                  Image.asset(
+                    'assets/icons/organizations.png',
+                    width: 60,
+                    height: 70,
+                  ),
+                  const SizedBox(width: 25),
+                  const Expanded(
+                    child: Text(
+                      'Организации',
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: RaisedButton(
+              elevation: 20,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(150)),
+              color: bgColorMapAppBar,
+              onPressed: () {},
+              child: Row(
+                children: [
+                  Image.asset(
+                    'assets/icons/map.png',
+                    width: 60,
+                    height: 70,
+                  ),
+                  const SizedBox(width: 25),
+                  const Expanded(
+                    child: Text(
+                      'Карта локаций',
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ]);
   }
 }
