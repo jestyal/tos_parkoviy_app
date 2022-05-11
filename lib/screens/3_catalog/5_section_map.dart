a4d6bd0460e043f57b15e2fb479f21c0d28d5c5e
Ekaterina Medvedeva <e.v.medvedeva.1996@yandex.ru>
2022-05-11 21:54:36 +0300

    fixed markers
    Signed-off-by: Ekaterina Medvedeva <e.v.medvedeva.1996@yandex.ru>

--- lib/screens/3_catalog/5_section_map.dart
+++ lib/screens/3_catalog/5_section_map.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tos_parkoviy_app/components/constants.dart';
 import 'package:flutter/services.dart';
-import 'package:tos_parkoviy_app/main.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 import 'dart:async';
