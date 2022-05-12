a473a760bc9e1b3457fd281ea51db2c1350dc9b8
Ekaterina Medvedeva <e.v.medvedeva.1996@yandex.ru>
2022-05-12 18:15:05 +0300

    added event, place, organization layouts
    Signed-off-by: Ekaterina Medvedeva <e.v.medvedeva.1996@yandex.ru>

--- lib/screens/3_catalog/3_catalog.dart
+++ lib/screens/3_catalog/3_catalog.dart
@@ -93,7 +93,7 @@ class _CatalogListState extends State<CatalogList> {
                         hintText: "Поиск",
                       ),
                       onChanged: (value) {
-                        setState((){
+                        setState(() {
                           searchString = value.toLowerCase();
                         });
                       },
@@ -117,115 +117,142 @@ class _CatalogListState extends State<CatalogList> {
                       left: 15, top: 20, right: 15, bottom: 15),
                   itemCount: items.length,
                   itemBuilder: (_, index) {
-                    return
-                      items[index].street!.toLowerCase().contains(searchString)
-                          || items[index].caretaker![0].caretakerDadname!.toLowerCase().contains(searchString)
-                          || items[index].caretaker![0].caretakerName!.toLowerCase().contains(searchString)
-                          || items[index].caretaker![0].caretakerSurname!.toLowerCase().contains(searchString)
-                      ?
-                      GestureDetector(
-                        child: Container(
-                          margin: const EdgeInsets.only(bottom: 15),
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius:
-                                const BorderRadius.all(Radius.circular(10)),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black.withOpacity(0.1),
-                                spreadRadius: 3,
-                                blurRadius: 5,
-                                offset: const Offset(0, 3),
-                              ),
-                            ],
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Row(
-                                children: [
-                                  Container(
-                                    width: 50,
-                                    height: 50,
-                                    padding: const EdgeInsets.all(2),
-                                    margin: const EdgeInsets.only(
-                                        right: 18, left: 15),
-                                    decoration: BoxDecoration(
-                                      color: this.data.colorSquare,
-                                      borderRadius:
-                                          const BorderRadius.all(Radius.circular(10)),
-                                    ),
-                                    child: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.center,
-                                      children: [
-                                        Text(
-                                          items[index].house.toString(),
-                                          textAlign: TextAlign.center,
-                                          style: const TextStyle(
-                                            fontSize: 16,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(
-                                        right: 10,
-                                        // left: 20,
-                                        top: 15,
-                                        bottom: 15),
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          items[index].street.toString(),
-                                          style: const TextStyle(
-                                            fontSize: 16,
-                                          ),
-                                        ),
-                                        Text(
-                                           items[index].caretaker![0].caretakerSurname.toString() + " " +
-                                               items[index].caretaker![0].caretakerName.toString() + " " +
-                                               items[index].caretaker![0].caretakerDadname.toString(),
-                                          style: const TextStyle(fontSize: 14),
-                                        ),
-                                      ],
-                                    ),
+                    return items[index]
+                                .street!
+                                .toLowerCase()
+                                .contains(searchString) ||
+                            items[index]
+                                .caretaker![0]
+                                .caretakerDadname!
+                                .toLowerCase()
+                                .contains(searchString) ||
+                            items[index]
+                                .caretaker![0]
+                                .caretakerName!
+                                .toLowerCase()
+                                .contains(searchString) ||
+                            items[index]
+                                .caretaker![0]
+                                .caretakerSurname!
+                                .toLowerCase()
+                                .contains(searchString)
+                        ? GestureDetector(
+                            child: Container(
+                              margin: const EdgeInsets.only(bottom: 15),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius:
+                                    const BorderRadius.all(Radius.circular(10)),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black.withOpacity(0.1),
+                                    spreadRadius: 3,
+                                    blurRadius: 5,
+                                    offset: const Offset(0, 3),
                                   ),
                                 ],
                               ),
-                              Column(
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
                                 children: [
-                                  Container(
-                                    width: 22,
-                                    height: 22,
-                                    margin: const EdgeInsets.only(right: 20),
-                                    child: const Align(
-                                      alignment: Alignment.center,
-                                      child: Icon(
-                                        Icons.arrow_forward_ios,
-                                        color: Colors.black87,
-                                        size: 20,
+                                  Row(
+                                    children: [
+                                      Container(
+                                        width: 50,
+                                        height: 50,
+                                        padding: const EdgeInsets.all(2),
+                                        margin: const EdgeInsets.only(
+                                            right: 18, left: 15),
+                                        decoration: BoxDecoration(
+                                          color: this.data.colorSquare,
+                                          borderRadius: const BorderRadius.all(
+                                              Radius.circular(10)),
+                                        ),
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.center,
+                                          children: [
+                                            Text(
+                                              items[index].house.toString(),
+                                              textAlign: TextAlign.center,
+                                              style: const TextStyle(
+                                                fontSize: 16,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
                                       ),
-                                    ),
+                                      Padding(
+                                        padding: const EdgeInsets.only(
+                                            right: 10,
+                                            // left: 20,
+                                            top: 15,
+                                            bottom: 15),
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              items[index].street.toString(),
+                                              style: const TextStyle(
+                                                fontSize: 16,
+                                              ),
+                                            ),
+                                            Text(
+                                              items[index]
+                                                      .caretaker![0]
+                                                      .caretakerSurname
+                                                      .toString() +
+                                                  " " +
+                                                  items[index]
+                                                      .caretaker![0]
+                                                      .caretakerName
+                                                      .toString() +
+                                                  " " +
+                                                  items[index]
+                                                      .caretaker![0]
+                                                      .caretakerDadname
+                                                      .toString(),
+                                              style:
+                                                  const TextStyle(fontSize: 14),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  Column(
+                                    children: [
+                                      Container(
+                                        width: 22,
+                                        height: 22,
+                                        margin:
+                                            const EdgeInsets.only(right: 20),
+                                        child: const Align(
+                                          alignment: Alignment.center,
+                                          child: Icon(
+                                            Icons.arrow_forward_ios,
+                                            color: Colors.black87,
+                                            size: 20,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
                                   ),
                                 ],
                               ),
-                            ],
-                          ),
-                        ),
-                        onTap: () => {
-                              colorappbar =
-                                  DataToCard(bgcolor: this.data.bgcolor),
-                              Navigator.pushNamed(
-                                  context, '/house_card_details',
-                                  arguments: colorappbar)
-                            })
-
+                            ),
+                            onTap: () => {
+                                  // colorappbar =
+                                  //     DataToCard(bgcolor: this.data.bgcolor),
+                                  Navigator.pushNamed(
+                                    context, '/house_card_details',
+                                    // arguments: colorappbar
+                                  )
+                                })
                         : Container();
                   },
                 );
