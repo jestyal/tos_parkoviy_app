7c274fdaad7265c8d212ae4394f6ea1393cafd0a
KHDiana <98642099+KHDiana@users.noreply.github.com>
2022-05-15 13:34:03 +0100

    Update 1_splash_screen.dart

--- lib/screens/1_splash_screen.dart
+++ lib/screens/1_splash_screen.dart
@@ -1,25 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:splashscreen/splashscreen.dart';
 import '2_homescreen.dart';
 
-class SplashScreenPage extends StatelessWidget {
-  const SplashScreenPage({Key? key}) : super(key: key);
+class SplashScreenPage extends StatefulWidget {
+  const SplashScreenPage({ Key? key }) : super(key: key);
 
   @override
+  State<SplashScreenPage> createState() => _SplashScreenPageState();
+}
+
+class _SplashScreenPageState extends State<SplashScreenPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 3), ()=> Navigator.push (
+      context, MaterialPageRoute(
+        builder: (context)=> ProfieleScreen())),);
+  }
+  
+ @override
   Widget build(BuildContext context) {
-    return SplashScreen(
-      seconds: 3,
-      navigateAfterSeconds: new ProfileScreen(),
-      backgroundColor: Color.fromARGB(255, 157, 233, 159),
-      title: new Text(
-        'Добро пожаловать в приложение ТОС "Парковый"',
-        textAlign: TextAlign.center,
-        textScaleFactor: 2,
-      ),
-      image: new Image.asset('assets/images/splashscreen.jpg'),
-      loadingText: Text("Загрузка"),
-      photoSize: 110.0,
-      loaderColor: Color.fromARGB(255, 244, 197, 54),
+    return Scaffold(
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/splashscreen.jpeg', height: 530, width: 450),
+            Text(" "),
+            Text('ТОС «Парковый»', style: TextStyle(fontSize: 25, color: Colors.green, fontStyle: FontStyle.italic), textAlign: TextAlign.end,),
+            Text(" "),
+            Text('Вместе сможем больше!', style: TextStyle(fontSize: 22, color: Colors.blue, fontStyle: FontStyle.italic),textAlign: TextAlign.end,)
+          ],
+        ),
+      )
+      
     );
   }
 }
